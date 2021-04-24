3c34bf2a6dca16fe5152746425077dc7a5a3700a
damnjoejoe <49086087+Yuqizhoujoe@users.noreply.github.com>
2021-04-24 01:14:08 -0500

    Delete GirlImageRepository.java

--- backend/src/main/java/joe/angular/sprintboot/AngularAndSpring/repository/GirlImageRepository.java
+++
@@ -1,7 +0,0 @@
-package joe.angular.sprintboot.AngularAndSpring.repository;
-
-import joe.angular.sprintboot.AngularAndSpring.entity.UserImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GirlImageRepository extends JpaRepository<UserImage, Long> {
-}
